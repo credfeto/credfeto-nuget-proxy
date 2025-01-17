5f5013f316eacc0575f287d6b1560eb142690deb
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-01-17 18:21:03 +0000

    Added details on where it was generated from

--- src/Credfeto.Nuget.Proxy/Helpers/StartupBanner.cs
+++ src/Credfeto.Nuget.Proxy/Helpers/StartupBanner.cs
@@ -6,6 +6,7 @@ internal static class StartupBanner
 {
     public static void Show()
     {
+        // Generated from https://fsymbols.com/generators/carty/
         const string banner = @"
 ███╗░░██╗██╗░░░██╗░██████╗░███████╗████████╗  ██████╗░██████╗░░█████╗░██╗░░██╗██╗░░░██╗
 ████╗░██║██║░░░██║██╔════╝░██╔════╝╚══██╔══╝  ██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝╚██╗░██╔╝
