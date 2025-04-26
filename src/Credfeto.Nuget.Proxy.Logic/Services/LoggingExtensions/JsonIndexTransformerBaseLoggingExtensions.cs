0fa1c6ae9c2e0d5f478b984d78b7836a177ee7cc
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 20:04:51 +0000

    Cleanup: JsonIndexTransformerBaseLoggingExtensions.cs

--- src/Credfeto.Nuget.Proxy.Logic/Services/LoggingExtensions/JsonIndexTransformerBaseLoggingExtensions.cs
+++ src/Credfeto.Nuget.Proxy.Logic/Services/LoggingExtensions/JsonIndexTransformerBaseLoggingExtensions.cs
@@ -11,21 +11,12 @@ internal static partial class JsonIndexTransformerBaseLoggingExtensions
         EventId = 1,
         Message = "Failed to retrieve JSON from {upstream} Received Http {statusCode}"
     )]
-    public static partial void UpstreamJsonFailed(
-        this ILogger logger,
-        Uri upstream,
-        HttpStatusCode statusCode
-    );
+    public static partial void UpstreamJsonFailed(this ILogger logger, Uri upstream, HttpStatusCode statusCode);
 
     [LoggerMessage(
         LogLevel.Information,
         EventId = 2,
         Message = "Retrieved JSON from {upstream} Received Http {statusCode} Length: {length}"
     )]
-    public static partial void UpstreamJsonOk(
-        this ILogger logger,
-        Uri upstream,
-        HttpStatusCode statusCode,
-        int length
-    );
+    public static partial void UpstreamJsonOk(this ILogger logger, Uri upstream, HttpStatusCode statusCode, int length);
 }
