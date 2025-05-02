71ca8df5c4882edb107ce8b8b2d45c90c88024fc
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-02 17:28:09 +0000

    Cleanup: JsonDownloader.cs

--- src/Credfeto.Nuget.Proxy.Logic/Services/JsonDownloader.cs
+++ src/Credfeto.Nuget.Proxy.Logic/Services/JsonDownloader.cs
@@ -21,11 +21,20 @@ public sealed class JsonDownloader : IJsonDownloader
         this._httpClientFactory = httpClientFactory;
     }
 
-    public async ValueTask<string> ReadUpstreamAsync(Uri requestUri, ProductInfoHeaderValue? userAgent, CancellationToken cancellationToken)
+    public async ValueTask<string> ReadUpstreamAsync(
+        Uri requestUri,
+        ProductInfoHeaderValue? userAgent,
+        CancellationToken cancellationToken
+    )
     {
         HttpClient client = this.GetClient(userAgent);
 
-        using (HttpResponseMessage result = await client.GetAsync(requestUri: requestUri, cancellationToken: cancellationToken))
+        using (
+            HttpResponseMessage result = await client.GetAsync(
+                requestUri: requestUri,
+                cancellationToken: cancellationToken
+            )
+        )
         {
             return result.IsSuccessStatusCode
                 ? await result.Content.ReadAsStringAsync(cancellationToken: cancellationToken)
@@ -36,13 +45,18 @@ public sealed class JsonDownloader : IJsonDownloader
     [DoesNotReturn]
     private static string Failed(Uri requestUri, HttpStatusCode resultStatusCode)
     {
-        throw new HttpRequestException($"Failed to download {requestUri}: {resultStatusCode.GetName()}", inner: null, statusCode: resultStatusCode);
+        throw new HttpRequestException(
+            $"Failed to download {requestUri}: {resultStatusCode.GetName()}",
+            inner: null,
+            statusCode: resultStatusCode
+        );
     }
 
     private HttpClient GetClient(ProductInfoHeaderValue? userAgent)
     {
-        return this._httpClientFactory.CreateClient(HttpClientNames.Json)
-                   .WithBaseAddress(this._config.UpstreamUrls[0])
-                   .WithUserAgent(userAgent);
+        return this
+            ._httpClientFactory.CreateClient(HttpClientNames.Json)
+            .WithBaseAddress(this._config.UpstreamUrls[0])
+            .WithUserAgent(userAgent);
     }
 }
