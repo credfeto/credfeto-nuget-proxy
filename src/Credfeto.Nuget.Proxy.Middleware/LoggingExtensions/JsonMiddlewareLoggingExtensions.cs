9dfd06e88257d3627703b83afb26e0370d062de1
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 04:30:00 +0000

    Cleanup: JsonMiddlewareLoggingExtensions.cs

--- src/Credfeto.Nuget.Proxy.Middleware/LoggingExtensions/JsonMiddlewareLoggingExtensions.cs
+++ src/Credfeto.Nuget.Proxy.Middleware/LoggingExtensions/JsonMiddlewareLoggingExtensions.cs
@@ -12,17 +12,42 @@ internal static partial class JsonMiddlewareLoggingExtensions
     [LoggerMessage(LogLevel.Warning, EventId = 2, Message = "Found {path} from upstream cache for {cacheSeconds}")]
     public static partial void FoundUpstreamJson(this ILogger<JsonMiddleware> logger, string path, long cacheSeconds);
 
-    [LoggerMessage(LogLevel.Warning, EventId = 3, Message = "Could not read {path} from upstream: {statusCode}, {message}.")]
-    public static partial void HttpError(this ILogger<JsonMiddleware> logger, string path, HttpStatusCode statusCode, string message, Exception exception);
+    [LoggerMessage(
+        LogLevel.Warning,
+        EventId = 3,
+        Message = "Could not read {path} from upstream: {statusCode}, {message}."
+    )]
+    public static partial void HttpError(
+        this ILogger<JsonMiddleware> logger,
+        string path,
+        HttpStatusCode statusCode,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Warning, EventId = 4, Message = "Invalid json {path} from upstream: {message}.")]
-    public static partial void InvalidJson(this ILogger<JsonMiddleware> logger, string path, string message, Exception exception);
+    public static partial void InvalidJson(
+        this ILogger<JsonMiddleware> logger,
+        string path,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Warning, EventId = 5, Message = "Too many requests {path} from upstream: {message}.")]
-    public static partial void TooManyRequests(this ILogger<JsonMiddleware> logger, string path, string message, Exception exception);
+    public static partial void TooManyRequests(
+        this ILogger<JsonMiddleware> logger,
+        string path,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Warning, EventId = 6, Message = "Internal server error {path} from upstream: {message}.")]
-    public static partial void InternalServerError(this ILogger<JsonMiddleware> logger, string path, string message, Exception exception);
+    public static partial void InternalServerError(
+        this ILogger<JsonMiddleware> logger,
+        string path,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Warning, EventId = 7, Message = "Could not read {path} from upstream (404 not found).")]
     public static partial void HttpNotFound(this ILogger<JsonMiddleware> logger, string path);
