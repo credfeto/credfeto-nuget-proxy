2fd12d24901ce23d0ce30022759b599651ec5154
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-05 04:32:14 +0000

    Cleanup: NuPkgMiddlewareLoggingExtensions.cs

--- src/Credfeto.Nuget.Proxy.Middleware/LoggingExtensions/NuPkgMiddlewareLoggingExtensions.cs
+++ src/Credfeto.Nuget.Proxy.Middleware/LoggingExtensions/NuPkgMiddlewareLoggingExtensions.cs
@@ -12,15 +12,40 @@ internal static partial class NuPkgMiddlewareLoggingExtensions
     [LoggerMessage(LogLevel.Warning, EventId = 2, Message = "Found {path} from upstream of {length} bytes.")]
     public static partial void FoundContent(this ILogger<NuPkgMiddleware> logger, string path, long length);
 
-    [LoggerMessage(LogLevel.Warning, EventId = 3, Message = "Could not read {path} from upstream: {statusCode}, {message}.")]
-    public static partial void HttpError(this ILogger<NuPkgMiddleware> logger, string path, HttpStatusCode statusCode, string message, Exception exception);
+    [LoggerMessage(
+        LogLevel.Warning,
+        EventId = 3,
+        Message = "Could not read {path} from upstream: {statusCode}, {message}."
+    )]
+    public static partial void HttpError(
+        this ILogger<NuPkgMiddleware> logger,
+        string path,
+        HttpStatusCode statusCode,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Warning, EventId = 4, Message = "Invalid json {path} from upstream: {message}.")]
-    public static partial void InvalidJson(this ILogger<NuPkgMiddleware> logger, string path, string message, Exception exception);
+    public static partial void InvalidJson(
+        this ILogger<NuPkgMiddleware> logger,
+        string path,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Warning, EventId = 5, Message = "Too many requests {path} from upstream: {message}.")]
-    public static partial void TooManyRequests(this ILogger<NuPkgMiddleware> logger, string path, string message, Exception exception);
+    public static partial void TooManyRequests(
+        this ILogger<NuPkgMiddleware> logger,
+        string path,
+        string message,
+        Exception exception
+    );
 
     [LoggerMessage(LogLevel.Warning, EventId = 6, Message = "Internal server error {path} from upstream: {message}.")]
-    public static partial void InternalServerError(this ILogger<NuPkgMiddleware> logger, string path, string message, Exception exception);
+    public static partial void InternalServerError(
+        this ILogger<NuPkgMiddleware> logger,
+        string path,
+        string message,
+        Exception exception
+    );
 }
