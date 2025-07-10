08bea9dda9a0d4099ea227dcabaccb5984f1f271
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-10 23:08:44 +0000

    Cleanup: JsonDownloaderLoggingExtensions.cs

--- src/Credfeto.Nuget.Proxy.Logic/Services/LoggingExtensions/JsonDownloaderLoggingExtensions.cs
+++ src/Credfeto.Nuget.Proxy.Logic/Services/LoggingExtensions/JsonDownloaderLoggingExtensions.cs
@@ -7,11 +7,33 @@ namespace Credfeto.Nuget.Proxy.Logic.Services.LoggingExtensions;
 
 internal static partial class JsonDownloaderLoggingExtensions
 {
-    [LoggerMessage(LogLevel.Information, EventId = 1, Message = "Retrieved {upstream} Metadata : ETag {etag}, Bytes: {contentLength}, Type: {contentType} Http Status {httpStatus}")]
-    private static partial void Metadata(this ILogger<JsonDownloader> logger, Uri upstream, string? etag, long contentLength, string? contentType, HttpStatusCode httpStatus);
+    [LoggerMessage(
+        LogLevel.Information,
+        EventId = 1,
+        Message = "Retrieved {upstream} Metadata : ETag {etag}, Bytes: {contentLength}, Type: {contentType} Http Status {httpStatus}"
+    )]
+    private static partial void Metadata(
+        this ILogger<JsonDownloader> logger,
+        Uri upstream,
+        string? etag,
+        long contentLength,
+        string? contentType,
+        HttpStatusCode httpStatus
+    );
 
-    public static void Metadata(this ILogger<JsonDownloader> logger, Uri upstream, in JsonMetadata metadata, HttpStatusCode httpStatus)
+    public static void Metadata(
+        this ILogger<JsonDownloader> logger,
+        Uri upstream,
+        in JsonMetadata metadata,
+        HttpStatusCode httpStatus
+    )
     {
-        logger.Metadata(upstream: upstream, etag: metadata.Etag, contentLength: metadata.ContentLength, contentType: metadata.ContentType, httpStatus: httpStatus);
+        logger.Metadata(
+            upstream: upstream,
+            etag: metadata.Etag,
+            contentLength: metadata.ContentLength,
+            contentType: metadata.ContentType,
+            httpStatus: httpStatus
+        );
     }
 }
