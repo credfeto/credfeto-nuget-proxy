5a4e543fd18fbcb95c21c7abd9fa32d6914248ff
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-07-10 23:04:40 +0000

    Cleanup: JsonDownloader.cs

--- src/Credfeto.Nuget.Proxy.Logic/Services/JsonDownloader.cs
+++ src/Credfeto.Nuget.Proxy.Logic/Services/JsonDownloader.cs
@@ -23,21 +23,34 @@ public sealed class JsonDownloader : IJsonDownloader
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger<JsonDownloader> _logger;
 
-    public JsonDownloader(ProxyServerConfig config, IHttpClientFactory httpClientFactory, ILogger<JsonDownloader> logger)
+    public JsonDownloader(
+        ProxyServerConfig config,
+        IHttpClientFactory httpClientFactory,
+        ILogger<JsonDownloader> logger
+    )
     {
         this._config = config;
         this._httpClientFactory = httpClientFactory;
         this._logger = logger;
     }
 
-    public async ValueTask<string> ReadUpstreamAsync(Uri requestUri, ProductInfoHeaderValue? userAgent, CancellationToken cancellationToken)
+    public async ValueTask<string> ReadUpstreamAsync(
+        Uri requestUri,
+        ProductInfoHeaderValue? userAgent,
+        CancellationToken cancellationToken
+    )
     {
         HttpClient client = this.GetClient(userAgent);
 
         // TODO: Load from cache...
         // TODO: If-None-Match: "33a64df551425fcc55e4d42a148795d9f25f89d4"
 
-        using (HttpResponseMessage result = await client.GetAsync(requestUri: requestUri, cancellationToken: cancellationToken))
+        using (
+            HttpResponseMessage result = await client.GetAsync(
+                requestUri: requestUri,
+                cancellationToken: cancellationToken
+            )
+        )
         {
             // if (result.StatusCode == HttpStatusCode.NotModified)
             // {
@@ -77,7 +90,10 @@ public sealed class JsonDownloader : IJsonDownloader
     {
         string? eTag = ExtractHeaderValue(headers: headers, name: "etag");
 
-        long contentLength = Convert.ToInt64(ExtractHeaderValue(headers: headers, name: "Content-Length") ?? "0", provider: CultureInfo.InvariantCulture);
+        long contentLength = Convert.ToInt64(
+            ExtractHeaderValue(headers: headers, name: "Content-Length") ?? "0",
+            provider: CultureInfo.InvariantCulture
+        );
 
         string? contentType = ExtractHeaderValue(headers: headers, name: "Content-Type");
 
@@ -102,13 +118,18 @@ public sealed class JsonDownloader : IJsonDownloader
     [DoesNotReturn]
     private static string Failed(Uri requestUri, HttpStatusCode resultStatusCode)
     {
-        throw new HttpRequestException($"Failed to download {requestUri}: {resultStatusCode.GetName()}", inner: null, statusCode: resultStatusCode);
+        throw new HttpRequestException(
+            $"Failed to download {requestUri}: {resultStatusCode.GetName()}",
+            inner: null,
+            statusCode: resultStatusCode
+        );
     }
 
     private HttpClient GetClient(ProductInfoHeaderValue? userAgent)
     {
-        return this._httpClientFactory.CreateClient(HttpClientNames.Json)
-                   .WithBaseAddress(this._config.UpstreamUrls[0])
-                   .WithUserAgent(userAgent);
+        return this
+            ._httpClientFactory.CreateClient(HttpClientNames.Json)
+            .WithBaseAddress(this._config.UpstreamUrls[0])
+            .WithUserAgent(userAgent);
     }
 }
